ca9b0489aab77ff56124b2359af0d4bb7377de81
RajarshiCode <rajarshidas47@gmail.com>
2024-08-01 15:27:26 +0530

    Just Checking, nothing else

---
+++ LearnCollectionFramework/res1/JustChecking.java
@@ -0,0 +1,7 @@
+package LearnCollectionFramework.res1;
+
+public class JustChecking {
+    public static void main(String[] args) {
+        System.out.println("Rajarshi Das is checking not it right now.");
+    }
+}
